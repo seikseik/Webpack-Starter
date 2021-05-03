952eb2d7294b84ca1117b0ac3b7f3883f86aaa0f
Matteo Sacchi <matteoseik@hotmail.it>
2021-05-03 14:15:10 +0100

    header

--- src/js/animations.js
+++ src/js/animations.js
@@ -27,25 +27,24 @@ window.addEventListener("scroll", () => {
   lastScroll = currentScroll;
 });
 
-function headerMenu() {
-  console.log("ciao");
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
 
-document.getElementById("menu").addEventListener("click", function(){
-  console.log("ciao");
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-});
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-item");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
 
 
 
